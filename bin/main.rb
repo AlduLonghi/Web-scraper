4f8575a09e2b89d210cf7dafe0164653be781439
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-08-11 07:02:39 -0300

    refactoring methods to keep the DRY rule

--- bin/main.rb
+++ bin/main.rb
@@ -7,22 +7,32 @@ class ScrapUI
   end
 
   def link
+    puts 'Please, enter the url you want to scrap from: '
     loop do 
-      puts 'Please, enter the url you want to scrap from: '
       @url = gets.chomp
       if @url.include? 'https://www.bitdegree.org/search?'
         file_name
         break
       else 
-        puts "invalid link"
+        puts "You have entered an invalid link. Please try again"
         next
       end
     end
   end
 
   def file_name
-    puts 'Please, enter the name of the file which stores the info: '
+    puts 'Please, enter the name of the file which stores the info (e.g. \'ruby-data\'): '
+    loop do
       file_n = gets.chomp
+      if file_n == nil
+        puts 'It seems you haven\'t entered any file name. Please, try again'
+        next
+      else
+        ScrapLogic.new(@url, file_n)
+        break
+      end
+    end
+  end
 end
 
 ScrapUI.new
