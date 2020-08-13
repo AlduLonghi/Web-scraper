91e8131a4fb767258bdd03c8c18933b3db189baf
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-08-13 10:17:08 -0300

    delete unnecesary file

--- spec/mock-tests/lalal.rb
+++
@@ -1,6 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-doc = Nokogiri::HTML(URI.parse("https://www.bitdegree.org/search?q=ruby&src=ukw&page=1#").open)
-File.write("parsed.html", doc, mode: "a")
-
